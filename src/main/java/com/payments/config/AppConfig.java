cad0659d8ddbb317ae8b628fe06f877aa8ec1865
ankit_rawat <ankitrawat741@gmail.com>
2025-10-25 15:01:41 +0530

    update project

--- src/main/java/com/payments/config/AppConfig.java
+++ src/main/java/com/payments/config/AppConfig.java
@@ -1,27 +1,7 @@
 package com.payments.config;
 
-import org.apache.coyote.ProtocolHandler;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
-
-import java.util.concurrent.Executors;
 
 public class AppConfig {
 
-    private static void customize(ProtocolHandler protocolHandler) {
-        protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-    }
 
-    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
-    public AsyncTaskExecutor asyncTaskExecutor() {
-        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
-    }
-
-    @Bean
-    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
-        return AppConfig::customize;
-    }
 }
